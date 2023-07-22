b2c7f6873a6e8330a4e485dbc3d431dcbafc0bf7
Duc7111 <phantrungduc2003@gmail.com>
2023-07-22 16:24:03 +0700

    Home screen header and loyalty card

--- app/src/main/java/com/example/midtermproject/MainActivity.kt
+++ app/src/main/java/com/example/midtermproject/MainActivity.kt
@@ -3,23 +3,28 @@ package com.example.midtermproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -29,6 +34,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.midtermproject.ui.theme.MidtermProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,19 +47,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SplashScreen(Modifier)
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Composable
@@ -65,102 +63,11 @@ fun SplashScreen(modifier: Modifier = Modifier) {
             .background(color = Color(0xff324a59).copy(alpha = 0.86f))
     ) {
         Image(
-            painter = painterResource(id = R.drawable.rizky-subagja-1k7tnx5gaww-unsplash1),
-            contentDescription = "rizky-subagja-1k7TnX5GAww-unsplash 1",
-            modifier = Modifier
-                .align(alignment = Alignment.TopStart)
-                .offset(x = (-1336).dp,
-                    y = (-1576).dp)
-                .width(width = 2731.dp)
-                .height(height = 4096.dp))
-        Image(
-            painter = painterResource(id = R.drawable.rizky-subagja-cd7bwkq3xtu-unsplash1),
+            painter = painterResource(id = R.drawable.splash_screen_background),
             contentDescription = "rizky-subagja-Cd7bWkq3XtU-unsplash 1",
             modifier = Modifier
                 .width(width = 375.dp)
                 .height(height = 812.dp))
-        Image(
-            painter = painterResource(id = R.drawable.rectangle2588),
-            contentDescription = "Rectangle 2588",
-            modifier = Modifier
-                .width(width = 375.dp)
-                .height(height = 812.dp)
-                .background(color = Color(0xff1d2335).copy(alpha = 0.37f)))
-        Box(
-            modifier = Modifier
-                .width(width = 348.dp)
-                .height(height = 21.dp)
-        ) {
-            Text(
-                text = "9:41",
-                color = Color.White,
-                textAlign = TextAlign.Center,
-                style = TextStyle(
-                    fontSize = 15.sp,
-                    letterSpacing = (-0.3).sp),
-                modifier = Modifier
-                    .align(alignment = Alignment.CenterStart)
-                    .offset(x = 0.dp,
-                        y = 0.dp)
-                    .width(width = 54.dp))
-            Image(
-                painter = painterResource(id = R.drawable.cellularconnection),
-                contentDescription = "Cellular Connection",
-                colorFilter = ColorFilter.tint(Color.Black),
-                modifier = Modifier
-                    .width(width = 17.dp)
-                    .height(height = 11.dp)
-                    .background(color = Color.White))
-            Image(
-                painter = painterResource(id = R.drawable.wifi),
-                contentDescription = "Wifi",
-                colorFilter = ColorFilter.tint(Color.Black),
-                modifier = Modifier
-                    .width(width = 15.dp)
-                    .height(height = 11.dp)
-                    .background(color = Color.White))
-            Box(
-                modifier = Modifier
-                    .width(width = 24.dp)
-                    .height(height = 11.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.border),
-                    contentDescription = "Border",
-                    alpha = 0.35f,
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterEnd)
-                        .offset(x = (-2.3280372619628906).dp,
-                            y = 0.dp)
-                        .width(width = 22.dp)
-                        .height(height = 11.dp)
-                        .clip(shape = RoundedCornerShape(2.6666667461395264.dp))
-                        .border(border = BorderStroke(1.dp, Color.Black),
-                            shape = RoundedCornerShape(2.6666667461395264.dp)))
-                Image(
-                    painter = painterResource(id = R.drawable.capacity),
-                    contentDescription = "Capacity",
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterEnd)
-                        .offset(x = (-4.328037261962891).dp,
-                            y = 1.1920928955078125e-7.dp)
-                        .width(width = 18.dp)
-                        .height(height = 7.dp)
-                        .clip(shape = RoundedCornerShape(1.3333333730697632.dp))
-                        .background(color = Color.White))
-                Image(
-                    painter = painterResource(id = R.drawable.cap),
-                    contentDescription = "Cap",
-                    alpha = 0.4f,
-                    modifier = Modifier
-                        .align(alignment = Alignment.CenterEnd)
-                        .offset(x = 7.152557373046875e-7.dp,
-                            y = 2.384185791015625e-7.dp)
-                        .width(width = 1.dp)
-                        .height(height = 4.dp)
-                        .background(color = Color.White.copy(alpha = 0.4f)))
-            }
-        }
         Text(
             text = "Ordinary Coffee House",
             color = Color(0xfffffaf6),
@@ -170,12 +77,14 @@ fun SplashScreen(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Medium),
             modifier = Modifier
                 .align(alignment = Alignment.TopStart)
-                .offset(x = 48.dp,
-                    y = 417.dp)
+                .offset(
+                    x = 48.dp,
+                    y = 417.dp
+                )
                 .width(width = 279.dp)
                 .height(height = 24.dp))
         Image(
-            painter = painterResource(id = R.drawable.group7103),
+            painter = painterResource(id = R.drawable.splash_screen_icon),
             contentDescription = "Group 7103",
             colorFilter = ColorFilter.tint(Color(0xfffffaf6)),
             modifier = Modifier
@@ -184,16 +93,148 @@ fun SplashScreen(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
 @Composable
-private fun SplashScreenPreview() {
-    SplashScreen(Modifier)
+fun HomeScreen(modifier: Modifier = Modifier, username : String = "Guest", cnum : Int = 4)
+{
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth())
+    {
+        //Header
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 10.dp))
+        {
+            // Greeting
+            Column(
+                )
+            {
+                Text(
+                    text = "Good morning",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color(0xFFD8D8D8),))
+                Text(
+                    text = username,
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black,))
+            }
+            Row()
+            {
+                // Cart button
+                Button(
+                    onClick = {
+                        //To Cart
+                    },
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent))
+                {
+                    Image(
+                        painter = painterResource(id = R.drawable.cart),
+                        contentDescription = "Cart icon",
+                        modifier = Modifier
+                            .width(26.dp)
+                            .height(26.dp))
+                }
+                // Profile button
+                Button(
+                    onClick = {
+                        //To Profile
+                    },
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent))
+                {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile),
+                        contentDescription = "Profile icon",
+                        modifier = Modifier
+                            .width(26.dp)
+                            .height(26.dp))
+                }
+            }
+        }
+        //Loyalty card
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 10.dp)
+                .background(color = Color(0xFF324A59), shape = RoundedCornerShape(12.dp)))
+        {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp))
+            {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp, vertical = 10.dp))
+                {
+                    Text(
+                        text = "Loyalty card",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Color(0xFFD8D8D8)),)
+                    Text(
+                        text = "$cnum /8",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Color(0xFFD8D8D8)),)
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 10.dp)
+                        .background(color = Color.White, shape = RoundedCornerShape(12.dp)))
+                {
+                    for(i : Int in 1..8)
+                        Image(
+                            painter = if(i <= cnum)
+                                    painterResource(id = R.drawable.coffee_icon_1)
+                                else
+                                    painterResource(id = R.drawable.coffee_icon_0),
+                            contentDescription = "Coffee Icon",
+                            modifier = Modifier
+                                .padding(vertical = 20.dp, horizontal = 0.dp)
+                                .width(30.dp)
+                                .height(30.dp))
+
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun SplashScreenPreview() {
     MidtermProjectTheme {
-        Greeting("Android")
+        SplashScreen(Modifier)
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    MidtermProjectTheme {
+        HomeScreen(Modifier)
     }
 }
